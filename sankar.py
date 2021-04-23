27acd1854d91492b0d5ec23a6dd9183c3df52c60
sankar-97 <83077597+sankar-97@users.noreply.github.com>
2021-04-23 19:56:31 +0530

    Add files via upload

---
+++ sankar.py
@@ -0,0 +1,24 @@
+print("sankar")
+
+a = 5684
+b = 7548
+print (a*b)
+
+g = ('apple', 'banana')
+h = "Hello World"
+i = 33
+
+x = type(g)
+y = type(h)
+z = type(i)
+print(x)
+print(y)
+print(z)
+
+
+
+
+
+
+
+
